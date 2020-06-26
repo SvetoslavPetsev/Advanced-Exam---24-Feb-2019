1f533fe3b95a76db5ff44e1d180e2cf81e09b66c
Svetoslav Petsev <svetoslav_petsev@users.noreply.github.com>
2020-06-27 00:23:13 +0300

    Add project files.

---
+++ P01.Club Party/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace P01.Club_Party
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Hall
+    {
+
+        public Hall(char number)
+        {
+            this.Number = number;
+            this.Guests = new List<int>();
+        }
+        public char Number { get; set; }
+        public List<int> Guests { get; set; }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            int maxCapcity = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Stack<string> info = new Stack<string>(input);
+            Queue<char> hallsQueue = new Queue<char>();
+
+
+            while (info.Any())
+            {
+                string check = info.Peek();
+                char letter;
+
+                if (char.TryParse(check, out letter) && char.IsLetter(letter))
+                {
+                    hallsQueue.Enqueue(letter);
+                    info.Pop();
+                }
+
+                if (!hallsQueue.Any())
+                {
+                    info.Pop();
+                }
+
+                while (hallsQueue.Any())
+                {
+                    Hall currHall = new Hall(hallsQueue.Dequeue());
+                    bool isFull = false;
+                    while (!isFull && info.Any())
+                    {
+                        char people;
+                        if (char.TryParse(info.Peek(), out people) && char.IsLetter(people))
+                        {
+                            hallsQueue.Enqueue(people);
+                            info.Pop();
+                            continue;
+                        }
+
+                        int sum = currHall.Guests.Sum() + int.Parse(info.Peek());
+                        if (sum <= maxCapcity)
+                        {
+                            currHall.Guests.Add(int.Parse(info.Pop()));
+                        }
+                        else
+                        {
+                            isFull = true;
+                        }
+                    }
+                    if (isFull)
+                    {
+                        Console.WriteLine($"{currHall.Number} -> {string.Join(", ", currHall.Guests)}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ P02.TronRacers/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace P02.TronRacers
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int firstRow = 0;
+            int firstCow = 0;
+
+            int secondRow = 0;
+            int secondCol = 0;
+
+            char[,] field = new char[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    field[i, j] = input[j];
+
+                    if (input[j] == 'f')
+                    {
+                        firstRow = i;
+                        firstCow = j;
+                    }
+                    else if (input[j] == 's')
+                    {
+                        secondRow = i;
+                        secondCol = j;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdFirst = commands[0];
+                string cmdSecond = commands[1];
+
+                firstRow = MovePlayerRow(size, firstRow, cmdFirst);
+                firstCow = MovePlayerCol(size, firstCow, cmdFirst);
+
+                secondRow = MovePlayerRow(size, secondRow, cmdSecond);
+                secondCol = MovePlayerCol(size, secondCol, cmdSecond);
+
+                if (field[firstRow, firstCow] == '*')
+                {
+                    field[firstRow, firstCow] = 'f';
+                }
+
+                else if (field[firstRow, firstCow] == 's')
+                {
+                    field[firstRow, firstCow] = 'x';
+                    break;
+                }
+
+                if (field[secondRow, secondCol] == '*')
+                {
+                    field[secondRow, secondCol] = 's';
+                }
+
+                else if (field[secondRow, secondCol] == 'f')
+                {
+                    field[secondRow, secondCol] = 'x';
+                    break;
+                }
+
+            }
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write(field[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static int MovePlayerRow(int size, int row, string cmd)
+        {
+            if (cmd == "up")
+            {
+                if (row == 0)
+                {
+                    return size - 1;
+                }
+                row--;
+                return row;
+            }
+            else if (cmd == "down")
+            {
+                if (row == size - 1)
+                {
+                    return 0;
+                }
+                row++;
+                return row;
+            }
+            return row;
+        }
+        static int MovePlayerCol(int size, int col, string cmd)
+        {
+            if (cmd == "left")
+            {
+                if (col == 0)
+                {
+                    return size - 1;
+                }
+                col--;
+                return col;
+            }
+            else if (cmd == "right")
+            {
+                if (col == size - 1)
+                {
+                    return 0;
+                }
+                col++;
+                return col;
+            }
+            return col;
+        }
+    }
+}
